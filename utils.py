0dbab7ee210029584acc33a47c723cc5eb1bbde9
AlinaModule <tgaminooff1c1al2@gmail.com>
2024-10-30 15:23:28 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 import telebot
-from telebot import types
 
-def create_subscription_keyboard():
-    """Создает клавиатуру с кнопкой "Подписаться на канал"."""
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Подписаться на канал", url=f"https://t.me/{config.CHANNEL_ID}")
-    keyboard.add(url_button)
-    return keyboard
+def send_welcome(bot, chat_id, channel_username):
+    chat_member = bot.get_chat_member(channel_username, chat_id)
+    
+    if chat_member.status in ['member', 'administrator', 'creator']:
+        bot.send_message(chat_id, 
+                         "Благодарим за подписку на канал, для работы с ботом используйте команды в специальном списке.")
+    else:
+        markup = telebot.types.InlineKeyboardMarkup()
+        button = telebot.types.InlineKeyboardButton("Перейти к каналу", url=f"https://t.me/{channel_username[1:]}")
+        markup.add(button)
+
+        bot.send_message(chat_id, 
+                         "Для работы с ботом подпишитесь на канал ниже:", 
+                         reply_markup=markup)

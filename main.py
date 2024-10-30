5ef78338783bffd76c74bea931f7cfaae023c936
AlinaModule <tgaminooff1c1al2@gmail.com>
2024-10-30 23:31:15 +0600

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import telebot
 from config import TOKEN, CHANNEL_USERNAME
-from utils import send_welcome
+from utils import send_welcome, handle_hikka_button
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     send_welcome(bot, message.chat.id, CHANNEL_USERNAME)
+
+@bot.message_handler(func=lambda message: message.text == "Hikka-Telethon")
+def hikka_button_pressed(message):
+    handle_hikka_button(bot, message.chat.id)
 
 if __name__ == '__main__':
     bot.polling()

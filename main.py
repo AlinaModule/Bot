a12a856ee9be72dac64625a6535c032e31f0eb82
AlinaModule <tgaminooff1c1al2@gmail.com>
2024-10-31 19:49:21 +0600

    main.py

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import telebot
 from config import TOKEN, CHANNEL_USERNAME
 from utils import send_welcome, send_start_menu, create_back_button
 
-# Создаем объект бота
 bot = telebot.TeleBot(TOKEN)
 
-# Команда /start
 @bot.message_handler(commands=['start'])
 def start_command(message):
     send_welcome(bot, message.chat.id, CHANNEL_USERNAME)
     send_start_menu(bot, message.chat.id)
 
-# Обработчик нажатий на кнопки
 @bot.message_handler(func=lambda message: True)
 def handle_buttons(message):
     """Обработчик сообщений от пользователей."""
@@ -28,10 +25,10 @@ def handle_buttons(message):
         bot.send_message(chat_id, "🖥️ <b>Выберите юзербота и библиотеку:</b>",
                          reply_markup=keyboard, parse_mode='HTML')
     elif text == "Hikka-Telethon":
-        article_text = "Статья: [Telethon-про модули для Hikka](https://telegra.ph/Telethon-pro-moduli-dlya-Hikka-10-30)"
+        article_text = "🎆 <b>Статья:</b> <[Telethon-про модули для Hikka](https://telegra.ph/Telethon-pro-moduli-dlya-Hikka-10-30)>"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown", reply_markup=create_back_button_keyboard())
     elif text == "RimTUB-Pyrogram":
-        article_text = "Статья: [RimTUB-про то как написать для него модуль](https://telegra.ph/Pyrogram-pro-moduli-dlya-RimTUB-10-30)"
+        article_text = "💉 <b>Статья:</b> <[RimTUB-про то как написать для него модуль](https://telegra.ph/Pyrogram-pro-moduli-dlya-RimTUB-10-30)>"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown", reply_markup=create_back_button_keyboard())
 
     if text == "ТГ-боты":
@@ -42,16 +39,16 @@ def handle_buttons(message):
         keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.row(pyrogram_tgbots_button, telethon_tgbots_button, telebot_tgbots_button)
         keyboard.add(back_button)
-        bot.send_message(chat_id, "<b>Выберите библиотеку для написания бота:</b>",
+        bot.send_message(chat_id, "📚 <b>Выберите библиотеку для написания бота:</b>",
                          reply_markup=keyboard, parse_mode='HTML')
     elif text == "Pyrogram":
-        article_text = "Статья: [Pyrogram-про то как написать простого телеграм бота](https://telegra.ph/Pyrogram-razrabotka-bota-10-30)"
+        article_text = "📀 <b>Статья:</b> <[Pyrogram-про то как написать простого телеграм бота](https://telegra.ph/Pyrogram-razrabotka-bota-10-30)>"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown", reply_markup=create_back_button_keyboard())
     elif text == "Telethon":
-        article_text = "Статья: [Telethon-про то как написать тг-бота](https://telegra.ph/Telethon-kak-napisat-svoego-telegram-bota-10-30)"
+        article_text = "⚙️ <b>Статья:</b> <[Telethon-про то как написать тг-бота](https://telegra.ph/Telethon-kak-napisat-svoego-telegram-bota-10-30)>"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown", reply_markup=create_back_button_keyboard())
     elif text == "Telebot":
-        article_text = "Статья: [Telebot-о том как написать простого телеграм бота](https://telegra.ph/Telebot-razrabotka-bota-10-30)"
+        article_text = "✨ <b>Статья:</b> <[Telebot-о том как написать простого телеграм бота](https://telegra.ph/Telebot-razrabotka-bota-10-30)>"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown", reply_markup=create_back_button_keyboard())
 
 if __name__ == '__main__':

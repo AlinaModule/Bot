a3e9d17a90e6d9fc4c15990b3fff49e5acdf20ce
AlinaModule <tgaminooff1c1al2@gmail.com>
2024-10-31 15:09:46 +0600

    utils.py

--- utils.py
+++ utils.py
@@ -1,53 +1,67 @@
 import telebot
 
 def send_welcome(bot, chat_id, channel_username):
+    """Отправляет приветственное сообщение после проверки подписки."""
     try:
         chat_member = bot.get_chat_member(channel_username, chat_id)
-        
         if chat_member.status in ['member', 'administrator', 'creator']:
-            bot.send_message(chat_id, 
-                             "Благодарим за подписку на канал, для работы с ботом используйте команды в специальном списке.")
+            bot.send_message(
+                chat_id,
+                "Благодарим за подписку на канал, для работы с ботом используйте команды в специальном списке."
+            )
+    except Exception as e:
+        print(f'Ошибка при проверке подписки: {e}')
+
+
 def send_start_menu(bot, chat_id):
+    """Отправляет стартовое меню с кнопками выбора категории."""
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     userbots_button = telebot.types.KeyboardButton("Юзерботы")
     tg_bots_button = telebot.types.KeyboardButton("ТГ-боты")
-    
-    keyboard.row(userbots_button, tg_bots_button)
-    
+
+    keyboard.add(userbots_button, tg_bots_button)
+
     bot.send_message(chat_id, "Выберите категорию:", reply_markup=keyboard)
+
 
 @bot.message_handler(func=lambda message: True)
 def handle_buttons(message):
+    """Обработчик сообщений от пользователей."""
     chat_id = message.chat.id
     text = message.text
-    
+
     if text == "Юзерботы":
         hikka_telethon_button = telebot.types.KeyboardButton("Hikka-Telethon")
         rimtub_pyrogram_button = telebot.types.KeyboardButton("RimTUB-Pyrogram")
-        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).row(hikka_telethon_button, rimtub_pyrogram_button)
-        bot.send_message(chat_id, "<b>Выберите юзербота и библиотеку:</b>", reply_markup=keyboard)
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(hikka_telethon_button, rimtub_pyrogram_button)
+        bot.send_message(chat_id, "<b>Выберите юзербота и библиотеку:</b>",
+                         reply_markup=keyboard, parse_mode='HTML')
     elif text == "Hikka-Telethon":
         article_text = "Статья: [Telethon-про модули для Hikka](https://telegra.ph/Telethon-pro-moduli-dlya-Hikka-10-30)"
         bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
-        elif text == "RimTUB-Pyrogram":
-            article_text = "Статья: [RimTUB-про то как написать для него модуль](https://telegra.ph/Pyrogram-pro-moduli-dlya-RimTUB-10-30);"
-            bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
+    elif text == "RimTUB-Pyrogram":
+        article_text = "Статья: [RimTUB-про то как написать для него модуль](https://telegra.ph/Pyrogram-pro-moduli-dlya-RimTUB-10-30)"
+        bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
 
     if text == "ТГ-боты":
         pyrogram_tgbots_button = telebot.types.KeyboardButton("Pyrogram")
-        telethon_tgbots_button = telebot.types.KeyboardButtin(" Telethon")
-        telebot_tgbots_button = telebot.types.KeyboardButton(" Telebot")
-        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).row(pyrogram_tgbots_button, telethob_tgbots_button, telebot_tgbots_button)
-        bot.send_message(chat_id, "<b>Выберите библиотеку для написания бота:</b>", reply_markup=keyboard, parse_mode='HTML')
-        elif text == "Pyrogram":
-            article_text = "Статья: [Pyrogram-про то как написать простого телеграм бота](https://telegra.ph/Pyrogram-razrabotka-bota-10-30);"
-            bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse mode="Markdown")
-        elif text == "Telethon":
-            article_text = "Статья: [Telethon-про то как написать тг-бота](https://telegra.ph/Telethon-kak-napisat-svoego-telegram-bota-10-30);"
-            bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
-        elif text == "Telebot":
-            article_text = "Статья: [Telebot-о том как написать простого телеграм бота](https://telegra.ph/Telebot-razrabotka-bota-10-30);"
-            bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
+        telethon_tgbots_button = telebot.types.KeyboardButton("Telethon")
+        telebot_tgbots_button = telebot.types.KeyboardButton("Telebot")
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(pyrogram_tgbots_button, telethon_tgbots_button, telebot_tgbots_button)
+        bot.send_message(chat_id, "<b>Выберите библиотеку для написания бота:</b>",
+                         reply_markup=keyboard, parse_mode='HTML')
+    elif text == "Pyrogram":
+        article_text = "Статья: [Pyrogram-про то как написать простого телеграм бота](https://telegra.ph/Pyrogram-razrabotka-bota-10-30)"
+        bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
+    elif text == "Telethon":
+        article_text = "Статья: [Telethon-про то как написать тг-бота](https://telegra.ph/Telethon-kak-napisat-svoego-telegram-bota-10-30)"
+        bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
+    elif text == "Telebot":
+        article_text = "Статья: [Telebot-о том как написать простого телеграм бота](https://telegra.ph/Telebot-razrabotka-bota-10-30)"
+        bot.send_message(chat_id, article_text, disable_web_page_preview=True, parse_mode="Markdown")
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)

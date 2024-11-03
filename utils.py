636e42810f39c7f83b74c1d86e0e47c16595eee2
AlinaModule <tgaminooff1c1al2@gmail.com>
2024-11-03 12:37:55 +0600

    utils.py

--- utils.py
+++ utils.py
@@ -1,52 +1,48 @@
-import telebot
-from telebot import types
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def send_welcome(bot, chat_id, CHANNEL_USERNAME):
-  """Отправляет приветственное сообщение после проверки подписки."""
-  try:
-    chat_member = bot.get_chat_member(CHANNEL_USERNAME, chat_id)
-    if chat_member.status not in ['member', 'administrator', 'creator']:
-      keyboard = types.InlineKeyboardMarkup()
-      subscribe_button = types.InlineKeyboardButton(text="Подписаться", url=f"https://t.me/{CHANNEL_USERNAME}")
-      keyboard.add(subscribe_button)
-      bot.send_message(
-        chat_id,
-        "✨ Приветствую тебя в этом боте!\n\nС помощью него ты сможешь написать самого простого телеграм бота или модуль для какого-либо юзербота, не отрицаю что скоро в боте будет ещё много новых функций, но перед тем как ты познакомишься с материалами подпишись на мой канальчик по кнопке ниже ♡",
-        parse_mode='HTML',
-        reply_markup=keyboard
-      )
-    else:
-      bot.send_message(
-        chat_id,
-        "✨ Приветствую тебя в этом боте!\n\nС помощью него ты сможешь написать самого простого телеграм бота или модуль для какого-либо юзербота, не отрицаю что скоро в боте будет ещё много новых функций ♡",
-        parse_mode='HTML'
-      )
-  except Exception as e:
-    print(f'Ошибка при проверке подписки: {e}')
-      
-def o_bote(bot, chat_id):
-    """инфо о боте"""
-    bot.send_message(
-        chat_id,
-        "🌟 <b>Бот был написан на библиотеке Telethon и состоит из 5 файлов:</b>\n<code>main.py</code> - тут находится логика всех команд в обоих меню (боковом и нижнем)\n<code>utils.py</code> - в этом файле хранится большинство текстов и логика функций\n<code>requirements.txt</code> - файл с зависимостями, необходимых для верной работы бота\n<code>config.py</code> - тут хранится токен бота\n<code>README.md</code> - здесь должна быть информация о боте, но я не заполняла, мне было лень\n<b>Исходный код бота можете посмотреть</b> <a href='https://github.com/AlinaModule/bot'>здесь</a>",
-        parse_mode='HTML')
+def send_welcome(client, chat_id, CHANNEL_USERNAME):
+ try:
+  chat_member = client.get_chat_member(CHANNEL_USERNAME, chat_id)
+  if chat_member.status not in ['member', 'administrator', 'creator']:
+   keyboard = InlineKeyboardMarkup(
+     [[InlineKeyboardButton(text="Подписаться", url=f"https://t.me/{CHANNEL_USERNAME}")]]
+   )
+   client.send_message(
+    chat_id,
+    "✨ Приветствую тебя в этом боте!\n\nС помощью него ты сможешь написать самого простого телеграм бота или модуль для какого-либо юзербота, не отрицаю что скоро в боте будет ещё много новых функций, но перед тем как ты познакомишься с материалами подпишись на мой канальчик по кнопке ниже ♡",
+    parse_mode='HTML',
+    reply_markup=keyboard
+   )
+  else:
+   client.send_message(
+    chat_id,
+    "✨ Приветствую тебя в этом боте!\n\nС помощью него ты сможешь написать самого простого телеграм бота или модуль для какого-либо юзербота, не отрицаю что скоро в боте будет ещё много новых функций ♡",
+    parse_mode='HTML'
+   )
+ except Exception as e:
+  print(f'Ошибка при проверке подписки: {e}')
+   
+def o_bote(client, chat_id):
+  client.send_message(
+    chat_id,
+    "🌟 Бот был написан на библиотеке Telethon и состоит из 5 файлов:\nmain.py - тут находится логика всех команд в обоих меню (боковом и нижнем)\nutils.py - в этом файле хранится большинство текстов и логика функций\nrequirements.txt - файл с зависимостями, необходимых для верной работы бота\nconfig.py - тут хранится токен бота\nREADME.md - здесь должна быть информация о боте, но я не заполняла, мне было лень\nИсходный код бота можете посмотреть здесь",
+    parse_mode='HTML')
 
-def send_start_menu(bot, chat_id):
-    """Отправляет стартовое меню с кнопками выбора категории."""
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    userbots_button = telebot.types.KeyboardButton("Юзерботы")
-    tg_bots_button = telebot.types.KeyboardButton("ТГ-боты")
-
-    keyboard.add(userbots_button, tg_bots_button)
-
-    bot.send_message(chat_id, "💫 <b>Выберите направление:</b>", reply_markup=keyboard, parse_mode='HTML')
+def send_start_menu(client, chat_id):
+  keyboard = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="Юзерботы", callback_data="userbots"), InlineKeyboardButton(text="ТГ-боты", callback_data="tg_bots")]]
+  )
+  client.send_message(chat_id, "💫 Выберите направление:", reply_markup=keyboard, parse_mode='HTML')
 
 def create_back_button():
-    """Создает клавиатуру с одной кнопкой 'Назад'"""
-    back_button = telebot.types.KeyboardButton('Назад')
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(back_button)
-    return keyboard
+  back_button = InlineKeyboardButton('Назад', callback_data="back")
+  keyboard = InlineKeyboardMarkup(
+    [[back_button]]
+  )
+  return keyboard
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+  app = Client("my_bot", bot_token=TOKEN)
+  app.run()
+    
